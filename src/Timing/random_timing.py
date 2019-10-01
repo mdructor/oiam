56fef03da6f5ab02bd9a52356d3cd0fc13c78917
GriffinBMeyers <griffin.b.meyers-1@ou.edu>
2019-09-30 21:34:20 -0500

    Changed output to correct format

--- src/Timing/random_timing.py
+++ src/Timing/random_timing.py
@@ -2,17 +2,24 @@ import requests
 import time
 
 with open('urls.txt', 'r') as f:
-    URLS = [line.strip() for line in f]
+    LINES = [line.strip() for line in f]
+
 
 def time_requests():
-    for i in URLS:
+    for i in LINES:
+
+        split = (i).split('@')
+        URLS = split[1]
         t1 = time.time()
-        r = requests.get(i)
+        r = requests.get(URLS)
+
         if r.status_code == 200:
             t2 = time.time()
-            print(i + ': request took ' + str(t2-t1) + ' seconds. Number: ' + r.text.strip())
+            file.write(i + ' ' + str(t2-t1) + ' ' + r.text.strip() + '\n')
         else:
-            print(i + ': status_code ' + str(status_code))
+            print(URLS + ': status_code ' + str(r.status_code))
 
 if __name__ == '__main__':
+    file = open("TimingResults.txt", 'a')
     time_requests()
+    file.close()
